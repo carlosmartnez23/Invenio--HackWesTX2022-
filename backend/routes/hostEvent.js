1620158279a312fb95249fea40691370c9274079
Carlos Martinez <carlosmartnez83@gmail.com>
2022-02-26 14:21:32 -0600

    write to json

--- backend/routes/hostEvent.js
+++ backend/routes/hostEvent.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 require('dotenv').config();
 
 module.exports = (app) => {
@@ -5,13 +6,13 @@ module.exports = (app) => {
     app.get("/hostEvent", (req, res) => {
         const file_name = process.env.JSON_FILE;
         var json = require(`../${file_name}`);
-        fs.writeFile(`../${file_name}`, "HELLO", (err) => {
+        fs.writeFile(`test.json`, "HELLO", (err) => {
             if (err)
                 console.log(err);
             else {
                 console.log("File written successfully\n");
                 console.log("The written has the following contents:");
-                console.log(fs.readFileSync(`../${file_name}`, "utf8"));
+                console.log(fs.readFileSync(`test.json`, "utf8"));
             }
         });
         console.log(req.data)
