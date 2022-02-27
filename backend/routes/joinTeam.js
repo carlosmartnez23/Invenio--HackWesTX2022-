720e4d2ed7fe34bd128b1c31d735da7949b467e0
anlqt89 <82059787+anlqt89@users.noreply.github.com>
2022-02-26 20:26:12 -0600

    asd
    asd

--- backend/routes/joinTeam.js
+++ backend/routes/joinTeam.js
@@ -6,41 +6,38 @@ eventId = 0;
 teamId = 0;
 
 member = {
-    memberId: 10,
-    name: "Name 10",
-    title: "title 10",
-    description: "10"
+    teamIdm: teamId,
+    name: "Name 10"
 }
 
 console.log(" weid");
 module.exports = (app) => {
- //get the team id and add the member to the array
+    //get the team id and add the member to the array
 
 
 
- app.get("/joinTeam", (request, response) => {
-     const fileName = process.env.JSON_FILE;
-     console.log("sSOmething weid", member["memberId"] );
-     var json = require(`../${fileName}`);
+    app.get("/joinTeam", (request, response) => {
+        const fileName = process.env.JSON_FILE;
+        console.log("sSOmething weid", member["memberId"]);
+        var json = require(`../${fileName}`);
 
-     const strMembers = JSON.stringify(member);
-     json["events"][eventId]["teams"][teamId]["memberRequests"].push({
-         "memberId": member["memberId"],
-         "name": member["name"],
-         "title": member["title"],
-         "description": member["description"]
-     })
+        const strMembers = JSON.stringify(member);
 
-     fs.writeFile(`eventsData.json`, JSON.stringify(json), 'utf8', (err) => {
-         if (err)
-             console.log(err);
-         else {
-             console.log("File written successfully\n");
-         }
-     });
-     response.send(json["events"][eventId]["teams"][teamId]["memberRequests"]);
+        json["events"][eventId]["teams"][teamId]["memberRequests"].push({
+            "teamId": teamId,
+            "name": member["name"]
+        });
+
+        fs.writeFile(`eventsData.json`, JSON.stringify(json), 'utf8', (err) => {
+            if (err)
+                console.log(err);
+            else {
+                console.log("File written successfully\n");
+            }
+        });
+        response.send(json["events"][eventId]["teams"][teamId]["memberRequests"]);
     });
 
-    
+
 
 }
