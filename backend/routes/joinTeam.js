d7aadecbc2d29dfbd86c90aefb2fb4560b622036
anlqt89 <82059787+anlqt89@users.noreply.github.com>
2022-02-26 19:31:30 -0600

    Update joinTeam.js
    sd

--- backend/routes/joinTeam.js
+++ backend/routes/joinTeam.js
@@ -2,36 +2,43 @@
 var fs = require('fs');
 require('dotenv').config();
 
-const cors = require('cors');
-console.log("sSOmething");
+eventId = 0;
+teamId = 0;
+
+member = {
+    memberId: 10,
+    name: "Name 10",
+    title: "title 10",
+    description: "10"
+}
+
 
 module.exports = (app) => {
  //get the team id and add the member to the array
 
- console.log(" weid");
+    console.log(" weid");
+
  app.get("/joinTeam", (request, response) => {
-    const fileName = process.env.JSON_TEAM_DATA;
-    console.log("sSOmething weid");
-    var json = require(`../${fileName}`);
-    var fileData = JSON.parse(json);
+     const fileName = process.env.JSON_FILE;
+     console.log("sSOmething weid", member["memberId"] );
+     var json = require(`../${fileName}`);
 
-        const strMembers= JSON.stringify(member);
+     const strMembers = JSON.stringify(member);
+     json["events"][eventId]["teams"][teamId]["memberRequests"].push({
+         "memberId": member["memberId"],
+         "name": member["name"],
+         "title": member["title"],
+         "description": member["description"]
+     })
 
-        json["events"][eventId]["teams"][teamId]["memberRequests"].push({
-        "memberId": member["memberID"],
-        "name": member["name"], 
-        "title": member["title"],
-        "description": member["description"]
-        })
-
-        fs.writeFile(`test.json`, JSON.stringify(json), 'utf8', (err) => {
-            if (err)
-                console.log(err);
-            else {
-                console.log("File written successfully\n");
-            }
-        });
-        response.send(json["events"][eventId]["teams"][teamId]["memberRequests"]);
+     fs.writeFile(`eventsData.json`, JSON.stringify(json), 'utf8', (err) => {
+         if (err)
+             console.log(err);
+         else {
+             console.log("File written successfully\n");
+         }
+     });
+     response.send(json["events"][eventId]["teams"][teamId]["memberRequests"]);
     });
 
     
