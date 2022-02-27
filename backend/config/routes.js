1b98e414cc9aac79cf0a7185106a6c62bf3599ad
Joe Cowles <JoeCowles@users.noreply.github.com>
2022-02-26 18:22:47 -0600

    Everything in createtean and hostEvent is working!

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -2,5 +2,5 @@ module.exports = function (app) {
 
     require("../routes/hostEvent.js")(app);
     require("../routes/hostTeam.js")(app);
-
+    require("../routes/createTeam.js")(app); 
 };
--- backend/routes/createTeam.js
+++ backend/routes/createTeam.js
@@ -1,40 +1,30 @@
 
 
 // Dependencies
-var fs = require('fs');
 
-
-
-
-const cors=require('cors');
+require('dotenv').config();
 
 module.exports = (app) =>{
-    require('dotenv').config();
-    //var fileData;
-    /*fs.readFile(filePath, 'utf8', (error, data) => {
-        if(error){
-        console.log(error);
-        return;
-        }
-        fileData = JSON.parse(data);
-        
-    })*/
+
+    var fs = require('fs');
+    const cors=require('cors'); 
     
     app.post('/createTeam', (request, response) => {
         
-        const fileName = process.env.JSON_TEAM_DATA;
-    
-        var json = require(`../${fileName}`);
-        var fileData = JSON.parse(json);
-        let buffer = '';
+        const fileName = process.env.JSON_FILE;
         console.log("Post request on /createTeam");
+        var fileData = require(`../${fileName}`);
+        console.log(fileData);
+
+        let buffer = '';
+        var obj;
         request.on('data', chunk => {
             buffer += chunk;
         });
         request.on('end', () => {
             // POST request body is now available as `buffer`
             console.log(buffer)
-            var obj = JSON.parse(buffer);
+            obj = JSON.parse(buffer);
             //console.log(fileData.events[0].name);
             for(var event of fileData.events){
                 
@@ -42,13 +32,14 @@ module.exports = (app) =>{
                     //console.log(event.name)
                     // Found the event, now add the team
                     event.teams.push(obj.team);
-                    fs.writeFileSync(fileName, JSON.stringify(fileData, null, 2), 'utf8');
-                    //console.log(event)
+                    fs.writeFileSync(`../${fileName}`, JSON.stringify(fileData, null, 2), 'utf8');
+                    console.log(event)
                 }
             }
         });
-
-        response.status(200).send(JSON.stringify(obj.team));      
+        response.status(200).end();
+        //response.sendStatus(200);
+        //response.status(200).send(JSON.stringify());      
     });
-}
+};
 
